770b09e77b4ffeff17d8e991a906e94c4eb4f2f8
Dominick Lee <d3lee@ucsd.edu>
2021-12-12 03:15:40 -0800

    ☕️ Added Hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import Avatar from "../images/avatar.jpeg"
+import Logo from "../images/logo.png"
+import "../styles/index.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <div className="Header">
+        <img src={Logo} alt="logo"></img>
+        <div className="HeaderText">
+          <a>About</a>
+          <a>Showcase</a>
+          <a>Contact</a>
+        </div>
+      </div>
+      <div className="Hero">
+        <div className="PhotoText">
+          <div className="HeroImage">
+            <img src={ Avatar } alt="Profile" className="profile-img" />
+          </div>
+          <div className="FancyRectangle"></div>
+          <div className="HeroText">
+            <h1>Chef <span>Alan</span></h1>
+            <h2>Senior Manager of R <span>&</span> D at the Cheesecake Factory</h2>
+          </div>
+        </div>
+      </div>
+      <div className="FancyDivider"></div>
+    </div>
+  );
 }
